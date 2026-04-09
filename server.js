581cf54958a0b6b2ffc8b3f9486ece103a12e4f7
guilhermealgustoribeiromattos-source <guilhermealgustoribeiromattos@gmail.com>
2026-04-09 17:25:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -82,6 +82,8 @@ app.post("/auth", (req, res) => {
   res.json({ success: true });
 });
 
-app.listen(3000, () => {
-  console.log("🔥 API rodando em http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("rodando");
 });
