49627663c3016bdf94b6d1639939e122a2a19144
guilhermealgustoribeiromattos-source <guilhermealgustoribeiromattos@gmail.com>
2026-04-10 21:38:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -59,13 +59,14 @@ app.get("/admin", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "admin.html"));
 });
 
-app.get("/login", (req, res) => {
+app.get("/discord-login", (req, res) => {
   const url =
     `https://discord.com/api/oauth2/authorize` +
     `?client_id=${encodeURIComponent(CLIENT_ID)}` +
     `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}` +
     `&response_type=code` +
     `&scope=identify`;
+
   res.redirect(url);
 });
 
